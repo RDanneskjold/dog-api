e18ea9ac80a4f2ac5910e17370432d1ceea8d7f8
Ben Tilghman <ben.tilghman@gmail.com>
2019-04-02 16:22:53 -0400

    done

--- index.js
+++ index.js
@@ -8,36 +8,61 @@ function getDogImage(number = 3) {
         .catch(error => alert('Something went wrong. Try again later.'));
 }
 
-function displayResults(responseJson) {
+function getBreedImage(breed) {
+    console.log(`fetching a pic of a ${breed}`);
     
+    fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+        .then(response => response.json())
+        .then(responseJson =>
+            displayResults(responseJson, breed))
+        .catch(error => alert("I'm sorry, we don't have any images of that breed. Please try another entry."));
+        
+}
+
+function displayResults(responseJson, breed) {
+ 
     let pictureArray = responseJson.message;
+   
+    if (breed) {
+        $('.results-header').html(`Look at this ${breed}!`);
 
-    const resultsString = generateResultsString(pictureArray);
+        $('.results').removeClass('hidden');
 
-    if(pictureArray.length > 1) {
+        return breedResults(pictureArray);
+    } else if (pictureArray.length > 1) {
         $('.results-header').html('Look at these dogs!');
     } else {
         $('.results-header').html('Look at this dog!');
     };
-    
+
+    const resultsString = generateResultsString(pictureArray);
+
     $('.results-list').html(resultsString);
     //display the results section
     $('.results').removeClass('hidden');
 }
 
+function breedResults(pic) {
+    
+    const breedPic = generateResultElement(pic);
+    $('.results-list').html(breedPic);
+}
+
 function generateResultsString(pictureArray) {
+    
     const items = pictureArray.map(picURL => generateResultElement(picURL));
     return items.join('');
 }
 
 function generateResultElement(picURL) {
+    
     return `
         <li class="js-dog-element">    
             <img src="${picURL}" class="results-img">
         </li>`;
 }
 
-function watchForm() {
+function watchNumberForm() {
     $('#js-dogs-form').on('submit', event => {
         event.preventDefault();
        
@@ -53,7 +78,21 @@ function watchForm() {
     });
 }
 
+function watchBreedForm() {
+    $('#js-breed-form').submit(event => {
+        event.preventDefault();
+
+        const breed = $('#breed').val();
+        
+        getBreedImage(breed);
+
+        $('#breed').val('');
+    });
+}
+
+
 $(function () {
     console.log('App loaded! Waiting for submit!');
-    watchForm();
+    watchNumberForm();
+    watchBreedForm();
 });
